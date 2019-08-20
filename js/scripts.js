9bde635674695b17b33e9c69cb7ceb572c358fad
Bahay Gulle Bilgi and Makijane Gilbert <makijane@>
2019-08-20 10:44:13 -0700

    Change forEach to for loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
-$(document).ready(function(event) {
+$(document).ready(function(flavor) {
   var flavors = ["Cookie Dough", "Cookies And Cream", "Strawberry", "Rainbow Sherbet."];
-  flavors.forEach(function(flavor) {
-    $("#icecream").append("<li>" + flavors + "</li>");
+  //flavors.forEach(function(flavor) {
+    //$("#icecream").append("<li>" + flavors + "</li>");
+    for (var index = 0; index < flavors.length; index += 1) {
+      $("#icecream").append("<li>" + flavors[index] + "</li>");
 
-  event.preventDefault();
-  });
+    }
+//  event.preventDefault();
 });
